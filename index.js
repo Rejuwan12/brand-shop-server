36ca299dbf9ea18f7175865567ac59029d4f1536
Rejuwane <rezwanrian1@gmail.com>
2023-10-20 02:55:05 +0600

    delete oparetion successfully

--- index.js
+++ index.js
@@ -91,6 +91,16 @@ app.post("/cart", async (req, res) => {
     res.send(result)
   })
 
+  // delete
+
+  app.delete("/cart/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: id }
+    const result = await cartCollection.deleteOne(query)
+    console.log(id);
+    res.send(result)
+  })
+
 
 
 
